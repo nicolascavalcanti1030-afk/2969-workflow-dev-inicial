51fcb64345b6e10651a0b9f846b9fb91dfb83e64
Nicthi <nicolascavalcanti1030@gmail.com>
2026-07-19 17:03:41 +0100

    test: add: tests for the /events route

---
+++ src/test/routes/eventosRoutes.test.js
@@ -0,0 +1,28 @@
+import { after } from 'mocha';
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+import app from '../../app.js';
+import db from '../../db/dbconfig.js';
+
+chai.use(chaiHttp);
+const { expect } = chai;
+
+after(async () => {
+  await db.destroy();
+});
+
+describe('GET em /eventos', () => {
+  it('deve retornar uma lista de eventos', (done) => {
+    chai.request(app)
+      .get('/eventos')
+      .set('Accept', 'appication/json')
+      .end((err, res) => {
+        expect(res.status).to.equal(200);
+        expect(res.body).to.be.an('array');
+        expect(res.body[0]).to.have.property('id');
+        expect(res.body[0]).to.have.property('nome');
+        expect(res.body[0]).to.have.property('descricao');
+        done();
+      });
+  });
+});
